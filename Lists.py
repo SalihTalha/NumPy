fefb1fcebae2b2096f266e1aaab346d6816727ab
SalihTalha <slhtlh1907@gmail.com>
2019-10-09 23:53:20 +0300

    Listlerle 1 2 ve 3 boyutlu elemanlar oluşturma

---
+++ Lists.py
@@ -0,0 +1,15 @@
+# Numpy kütüphanesinden bazı örnekler
+# Bunları kendiniz denemeniz önerilir
+import numpy as np
+
+# 1 boyutlu ve 2 boyutlu list
+a = np.array([1, 2, 3])
+b = np.array([[9, 8, 7], [6, 5, 4]])
+
+print("a:\n" , a )
+print("b:\n" , b)
+
+# 3 Boyutlu bir list
+c = np.array([ [[1,2,3],[4,5,6]] , [[7,8,9],[10,11,12]] ])
+
+print("c:\n" , c)
